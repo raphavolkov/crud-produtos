a9f326d9a53ea6d2fd9e7781121d4234fe5dab22
raphavolkov <raphavolkov@proton.me>
2026-03-06 11:44:44 -0300

    first commit

---
+++ src/test/java/com/raphavolkov/crud_produtos/CrudProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphavolkov.crud_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
